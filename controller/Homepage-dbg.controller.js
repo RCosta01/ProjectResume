dae0192a12049c59f644095b396e7e9aed23cc68
RCosta01 <rafael.sa.costa2001@gmail.com>
2025-09-03 14:10:48 +0100

    Updates

--- controller/Homepage-dbg.controller.js
+++ controller/Homepage-dbg.controller.js
@@ -10,10 +10,23 @@ sap.ui.define([
             oRouter.getRoute("RouteHomepage").attachMatched(function (oEvent) {
                 sap.ui.getCore().applyTheme("sap_fiori_3_dark");
             }, this);
+            //window.scrollTo(0, 0);
         },
 
         onNav: function () {
             this.getOwnerComponent().getRouter().navTo('routePage1');
+        },
+
+        onNavDemos: function(oEvent){
+            const source = oEvent.getSource()
+            const text = source.getText()
+            switch (text) {
+                case 'PDP':
+                    this.onNavPDP()
+                    break;
+                default:
+                    break;
+            }
         },
 
         onNavPDP: function () {
@@ -23,6 +36,45 @@ sap.ui.define([
         onLinkpress: function (oEvent) {
             console.log('event Fired!')
             const demo = oEvent.getSource().getBindingContext("Timeline").getProperty('demo')
+            switch (demo) {
+                case 'PDP':
+                    this.onNavPDP()
+                    break;
+                default:
+                    break;
+            }
+        },
+
+        onOpenGithub: function() {
+            window.open("https://github.com/RCosta01", "_blank")
+        },
+
+        /**
+         * @override
+         * @returns {void|undefined}
+         */
+        onAfterRendering: function() {
+            const demosItems = this.byId("demosGrid").getContent();
+            for(let i = 0; i < demosItems.length; i++){
+                const button = demosItems[i].getItems()[0].getItems()[0];
+                const styleClass = demosItems[i].getBindingContext("demos").getProperty("css");
+                if(styleClass) button.addStyleClass(styleClass);
+            }
+        },
+
+        navToExp: function() {
+            const profHist = this.byId("professionalHistorySection")
+            if(profHist) profHist.$()[0].scrollIntoView({ behavior: "smooth", block: "start" })
+        },
+
+        navToDemos: function() {
+            const demos = this.byId("demosSection")
+            if(demos) demos.$()[0].scrollIntoView({ behavior: "smooth", block: "start" })
+        },
+
+        navToProjects: function() {
+            const projects = this.byId("projectsSection")
+            if(projects) projects.$()[0].scrollIntoView({ behavior: "smooth", block: "start" })
         }
     });
 });
---
+++ controller/components/TimelineItem-dbg.js
@@ -0,0 +1,100 @@
+sap.ui.define([
+    "sap/ui/core/Control",
+    "sap/m/Link",
+    "sap/ui/Device"
+], function (
+    Control,
+    Link,
+    Device
+) {
+    "use strict";
+
+    return Control.extend("projects.controller.components.TimelineItem", {
+        metadata: {
+            properties: {
+                title: { type: "string" },
+                subtitle: { type: "string" },
+                description: { type: "string" },
+                date: { type: "string" },
+                cssClass: { type: "string", defaultValue: "" },
+                demo: { type: "string", defaultValue: "" },
+            },
+            events: {
+                linkPress: {}
+            }
+        },
+
+        renderer: {
+            apiVersion: 2,
+            render: function (oRm, oControl) {
+
+                oRm.openStart("div", oControl);
+                oRm.class("project-content");
+                if (oControl.getCssClass()) {
+                    oRm.class(oControl.getCssClass());
+                }
+                oRm.openEnd();
+
+                if (oControl.getTitle()) {
+                    oRm.openStart("h2");
+                    oRm.openEnd();
+                    oRm.text(oControl.getTitle());
+                    oRm.close("h2");
+                }
+
+                if (oControl.getSubtitle()) {
+                    oRm.openStart("h2");
+                    oRm.openEnd();
+                    oRm.text(oControl.getSubtitle());
+                    oRm.close("h2");
+                }
+
+                if (oControl.getDemo()) {
+                    const oLink = new Link({
+                        id: oControl.getDemo() + '-demo',
+                        text: 'Demo',
+                        press: function(oEvent) {
+                            oControl.fireLinkPress();
+                        }
+                    }).addStyleClass("timelineLinkAsH2")
+                    oLink.addDelegate({
+                        onAfterRendering: function() {
+                            $(oControl.getDemo() + '-demo').attr("tabindex", -1);
+                        }
+                    })
+                    oRm.renderControl(oLink);
+                }
+
+                if (oControl.getDescription()) {
+                    oRm.openStart("p");
+                    oRm.openEnd();
+                    oRm.text(oControl.getDescription());
+                    oRm.close("p");
+                }
+
+                oRm.close("div"); // content
+            }
+        },
+
+        /**
+         * @override
+         * @param {jQuery.Event} oEvent <p>onAfterRendering event object</p>
+         * @returns {void|undefined}
+         */
+        onAfterRendering: function (oEvent) {
+            const domRef = this.getDomRef();
+            if (!domRef) return;
+            const threshold = Device.system.phone ? 0.4 : 0.6;
+            const observer = new IntersectionObserver((entries) => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        domRef.classList.add("projectVisible");
+                    } else {
+                        domRef.classList.remove("projectVisible");
+                    }
+                })
+            }, { threshold: threshold })
+            observer.observe(domRef);
+        }
+    });
+});
